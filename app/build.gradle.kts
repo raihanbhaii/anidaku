a9465be7e5034da59c728aecd004bcfeb4c6f39e
Raihan <tipuraihanff@gmail.com>
2026-04-01 21:22:26 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    // Kotlin serialization plugin for @Serializable support
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 android {
@@ -41,26 +43,30 @@ android {
 }
 
 dependencies {
+    // Core & Lifecycle
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")
 
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2025.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material:material-icons-extended")
-
     implementation("androidx.navigation:navigation-compose:2.9.0")
 
+    // Image Loading
     implementation("io.coil-kt:coil-compose:2.7.0")
 
+    // Media3 (ExoPlayer)
     implementation("androidx.media3:media3-exoplayer:1.6.1")
     implementation("androidx.media3:media3-exoplayer-hls:1.6.1")
     implementation("androidx.media3:media3-ui:1.6.1")
     implementation("androidx.media3:media3-datasource:1.6.1")
 
+    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     // Ktor
@@ -69,4 +75,7 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:3.1.2")
     implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.2")
     implementation("io.ktor:ktor-client-logging:3.1.2")
+
+    // Kotlinx Serialization Runtime (NEW)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
