0980ca3fa02e5b1b857b539d2d7186ebd8dbb955
Raihan <tipuraihanff@gmail.com>
2026-04-02 08:29:13 +0600

    Update NetworkClient.kt

--- app/src/main/java/com/anilite/data/NetworkClient.kt
+++ app/src/main/java/com/anilite/data/NetworkClient.kt
@@ -1,13 +1,12 @@
 package com.anilite.data
 
 import io.ktor.client.*
-import io.ktor.client.engine.android.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 object NetworkClient {
-    val client = HttpClient(Android) {
+    val client = HttpClient {
         install(ContentNegotiation) {
             json(Json {
                 ignoreUnknownKeys = true
