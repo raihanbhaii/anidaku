d8d0e90f4a1720da6ff184e41ea568d0e4095ee9
Raihan <tipuraihanff@gmail.com>
2026-04-03 11:59:52 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 35
         versionCode = 2
-        versionName = "1.5"
+        versionName = "1.6"
     }
     buildTypes {
         release {
