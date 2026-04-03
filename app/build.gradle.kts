d664c6ce9fdfd21b1dc4f1e30d6fb5d12cb81ddb
Raihan <tipuraihanff@gmail.com>
2026-04-03 11:26:24 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.anilite"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.2"
+        versionCode = 2
+        versionName = "1.4"
     }
     buildTypes {
         release {
