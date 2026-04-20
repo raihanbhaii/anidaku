f7009a8915f55721689b31ce0d2a6f22261f52e9
raihanbhaii <235073842+raihanbhaii@users.noreply.github.com>
2026-04-20 09:11:20 -0400

    Update: Switch build type from Debug to Release and update CI/CD workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
